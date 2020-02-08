0829a2e73424728a75e3529942c4910de0075550
Jerry Turcios <jerryturcios08@gmail.com>
2020-02-08 11:16:04 -0500

    Add table view data source and delegate methods

--- Package Dash/Screens/HomeScreen.swift
+++ Package Dash/Screens/HomeScreen.swift
@@ -9,8 +9,32 @@
 import UIKit
 
 class HomeScreen: UIViewController {
+    @IBOutlet var tableView: UITableView!
+
+    var tasks = [Task]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        #if DEBUG
+        // Add dummy data for tasks
+        #endif
+    }
+}
+
+extension HomeScreen: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Task", for: indexPath) as? TaskCell else {
+            fatalError("Error: Unable to dequeue table view cell for tasks")
+        }
+
+        let task = tasks[indexPath.row]
+        cell.setupCell(task: task)
+
+        return cell
     }
 }
